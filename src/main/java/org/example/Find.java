6561ac2322d48486985d357fca284b65649ca7b8
Gabriela Horčičková <cavilliera@centrum.cz>
2024-07-14 17:27:36 +0200

    Class Find - new method findMatching
    Create new  double method findMatchinf

--- src/main/java/org/example/Find.java
+++ src/main/java/org/example/Find.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.List;
+
 public class Find {
+    public static Double findMatching(
+            List<Double> list, Double lowerLimit, Double upperLimit
+    ) {
+        return list.get(0);
+    }
 }
