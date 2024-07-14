d6ba2502d29be0a852ce2cc2a7bb1014abe47fce
Gabriela Horčičková <cavilliera@centrum.cz>
2024-07-14 17:44:19 +0200

    Class FindMatchingTest - nová metoda middle1Test s anotací @Test
    Vytvoření nové metody middle1Test + importy List, Assertions, @Test

--- src/test/java/org/example/FindMatchingTest.java
+++ src/test/java/org/example/FindMatchingTest.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class FindMatchingTest {
+
+    @Test
+    public void middle1Test() {
+        List<Double> testList = List.of(10.0, 15.45, 5.811, 175.0);
+        Double testResult = Find.findMatching(testList, 4.0, 9.3);
+        Assertions.assertEquals(5.811, testResult);
+    }
 }
