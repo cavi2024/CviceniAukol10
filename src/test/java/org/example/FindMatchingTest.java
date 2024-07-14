119a445eaa19856a4469e84b8ed2e99bec0f83b4
Gabriela Horčičková <cavilliera@centrum.cz>
2024-07-14 19:11:05 +0200

    Class FindMatchingTest - přidání metody middleUkol2Test
    Metoda middleUkol2Test se seznamem hodnot, limity a očekávaným výsledkem

--- src/test/java/org/example/FindMatchingTest.java
+++ src/test/java/org/example/FindMatchingTest.java
@@ -28,4 +28,11 @@ public class FindMatchingTest {
         Double testResult = Find.findMatching(testList, 11.0, 12.6);
         Assertions.assertEquals(12.0, testResult);
     }
+
+    @Test
+    public void middleUkol2Test() {
+        List<Double> testList = List.of(12.0, 8.126, 9.0, 158.0, 6.0);
+        Double testResult = Find.findMatching(testList, 8.0, 10.0);
+        Assertions.assertEquals(8.126, testResult);
+    }
 }
