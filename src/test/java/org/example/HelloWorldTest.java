1553bd393aade82b748d19b1329709041a9d40fe
Gabriela Horčičková <cavilliera@centrum.cz>
2024-07-14 17:35:21 +0200

    Class HelloWorldTest - přidání metody printHelloTest s anotací
    Metoda printHelloTest s anotací @Test

--- src/test/java/org/example/HelloWorldTest.java
+++ src/test/java/org/example/HelloWorldTest.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 public class HelloWorldTest {
+
+    @Test
+    public void printHelloTest() {
+        System.out.println("Hello world!");
+    }
 }
