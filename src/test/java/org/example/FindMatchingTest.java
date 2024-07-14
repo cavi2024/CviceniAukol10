b8014fc1226593be73d3db552f84d8adb9b7cc60
Gabriela Horčičková <cavilliera@centrum.cz>
2024-07-14 18:07:30 +0200

    Class FindMatchingTest - vytvoření metody emptyTest
    Metoda emptyTest s pomocí metody Assertions.assertThrows pro výjimku

--- src/test/java/org/example/FindMatchingTest.java
+++ src/test/java/org/example/FindMatchingTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindMatchingTest {
@@ -12,5 +13,12 @@ public class FindMatchingTest {
         List<Double> testList = List.of(10.0, 15.45, 5.811, 175.0);
         Double testResult = Find.findMatching(testList, 4.0, 9.3);
         Assertions.assertEquals(5.811, testResult);
+    }
+
+    @Test
+    public void emptyTest() {
+        List<Double> testList = new ArrayList<>();
+        Assertions.assertThrows(
+                Exception.class, () -> Find.findMatching(testList, 10.0, 12.0));
     }
 }
